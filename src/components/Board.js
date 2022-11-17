1fe3e19b580dd81b18d1d8d369db2cf5876e0bc0
Julie Evans <juliemevans17@gmail.com>
2022-11-17 17:42:18 -0600

    Added alerts

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import React, { useCallback } from "react";
+import { Box, Button, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, useDisclosure } from "@chakra-ui/react";
 
 export default function Board() {
   // const board = Array.from(Array(9), (e,i))
   // const board = ["X", "", "", "", "", "", "", "", ""]
   const [board, setBoard] = React.useState(["", "", "", "", "", "", "", "", ""])
   const [turnCount, setTurnCount] = React.useState(0)
+
+  const [alert, setAlert] = React.useState(null)
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
 
   const winCombos = [
     [0,1,2],  // Top row
@@ -31,32 +36,67 @@ export default function Board() {
   })
   const isFull = turnCount === 9
   const isOver = isWin || isFull
-  const winner = isWin ? `${board[isWin[0]]} wins!` : ""
+  const winner = isWin ? `${board[isWin[0]]} wins!` : null
   
-  console.log(isOver && !isWin ? "Cat's game!" : winner)
+
+  React.useEffect(() => {
+    if(isOver) {
+      if(isWin) {
+        setAlert(`Congratulations ${board[isWin[0]]} wins!`)
+      } else {
+        setAlert("Cat's game!")
+      }
+    } 
+  }, [isOver])
+
+  React.useEffect(() => {
+    if(alert) onOpen()
+  }, [alert])
 
   function handleAddPiece(e) {
     const newBoard = [...board]
     const token = turnCount % 2 == 1 ? "O" : "X"
+
     if(!isOver) {
       if(newBoard[e.currentTarget.id] === "") {
         newBoard.splice(e.currentTarget.id, 1, token)
         if(turnCount < 9) setTurnCount(turnCount + 1)
       } else {
-        console.log("Can't move there!")
+        setAlert("Can't move there!")
+        onOpen()
       }
       setBoard(newBoard)
     }
   }
 
-  function handelReset() {
+  function handleReset() {
     setBoard(["", "", "", "", "", "", "", "", ""])
     setTurnCount(0)
+    setAlert(null)
   }
 
   return (
     <>
-      <Button onClick={handelReset} colorScheme="teal">Reset Board</Button>
+      <br/>
+      <Button onClick={handleReset} colorScheme="teal">Reset Board</Button>
+
+      <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              {alert}
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              {/* <Button ref={cancelRef} onClick={onClose}>
+                Close
+              </Button> */}
+              <Button onClick={onClose} ml={3}>
+                Close
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
       <br />
       <Box className="Board" bg="white">
         {board.map((el, index) => (
