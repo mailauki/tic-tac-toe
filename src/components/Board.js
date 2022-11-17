b88a634a753a9ee30da0768b11acea56643a5f4c
Julie Evans <juliemevans17@gmail.com>
2022-11-17 13:55:25 -0600

    Added placement function and reset button

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,36 +1,74 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 export default function Board() {
   // const board = Array.from(Array(9), (e,i))
   // const board = ["X", "", "", "", "", "", "", "", ""]
   const [board, setBoard] = React.useState(["", "", "", "", "", "", "", "", ""])
+  const [turnCount, setTurnCount] = React.useState(0)
 
-  console.log(board)
+  const winCombos = [
+    [0,1,2],  // Top row
+    [3,4,5],  // Middle row
+    [6,7,8],  // Bottom row
+    [0,3,6],  // Left collumn
+    [1,4,7],  // Middle collumn
+    [2,5,8],  // Right collumn
+    [2,4,6],  // Left diagonal
+    [0,4,8]   // Right diagonal
+  ]
+
+  const xArray = board.reduce((a,e,i) => e === "X" ? a.concat(i) : a, [])
+  const oArray = board.reduce((a,e,i) => e === "O" ? a.concat(i) : a, [])
+
+  const isWin = winCombos.find((win) => {
+    if(win.map((i) => xArray.includes(i)).every((el) => el === true)) {
+      return win
+    }
+    else if(win.map((i) => oArray.includes(i)).every((el) => el === true)) {
+      return win
+    }
+  })
+  const isFull = turnCount === 9
+  const isOver = isWin || isFull
+  const winner = isWin ? `${board[isWin[0]]} wins!` : ""
+  
+  console.log(isOver && !isWin ? "Cat's game!" : winner)
 
   function handleAddPiece(e) {
     const newBoard = [...board]
-    newBoard.splice(e.currentTarget.id, 1, "X")
-    setBoard(newBoard)
+    const token = turnCount % 2 == 1 ? "O" : "X"
+    if(!isOver) {
+      if(newBoard[e.currentTarget.id] === "") {
+        newBoard.splice(e.currentTarget.id, 1, token)
+        if(turnCount < 9) setTurnCount(turnCount + 1)
+      } else {
+        console.log("Can't move there!")
+      }
+      setBoard(newBoard)
+    }
+  }
+
+  function handelReset() {
+    setBoard(["", "", "", "", "", "", "", "", ""])
+    setTurnCount(0)
   }
 
   return (
-    <Box className="Board" bg="white">
-      {board.map((el, index) => (
-        <Box  
-          id={index} 
-          className="Box"
-          onClick={handleAddPiece}
-        >
-          <p className="Piece">{el}</p>
-          {/* <p className="Piece">{el === 1 || el === 4 || el === 8 ? "X" : "O"}</p> */}
-          {/* {el === 1 || el === 4 || el === 8 ? (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="Piece"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
-          ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="Piece"><path d="M12 0c-6.623 0-12 5.377-12 12s5.377 12 12 12 12-5.377 12-12-5.377-12-12-12zm0 22c-5.519 0-10-4.48-10-10 0-5.519 4.481-10 10-10 5.52 0 10 4.481 10 10 0 5.52-4.48 10-10 10z"/></svg>
-          )} */}
-        </Box>
-      ))}
-    </Box>
+    <>
+      <Button onClick={handelReset} colorScheme="teal">Reset Board</Button>
+      <br />
+      <Box className="Board" bg="white">
+        {board.map((el, index) => (
+          <Box  
+            id={index} 
+            className="Box"
+            onClick={handleAddPiece}
+          >
+            <p className="Piece">{el}</p>
+          </Box>
+        ))}
+      </Box>
+    </>
   )
 }
